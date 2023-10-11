da766629f3ac5d4be2ad40170f7efa93aae79b99
aca <112835299+harfley@users.noreply.github.com>
2023-10-11 08:37:14 +0700

    Add files via upload

---
+++ luas.js
@@ -0,0 +1,11 @@
+
+function hitung() {
+    document.getElementById('panjang').value;
+    document.getElementById('lebar').value;
+}
+
+function reset() {
+    document.getElementById('panjang').value = '';
+    document.getElementById('lebar').value = '';
+    document.getElementById('hasil').value = '';
+}
